c4a451bfcba31f30aa180b88e59c7eb7ef772878
Brendan Hay <brendan.g.hay@gmail.com>
2013-07-14 19:21:56 +0200

    Updating license header

--- src/Network/PagerDuty/Internal.hs
+++ src/Network/PagerDuty/Internal.hs
@@ -1,6 +1,6 @@
 -- |
 -- Module      : Network.PagerDuty.Internal
--- Copyright   : (c) 2013 Brendan Hay <brendan.g.hay@gmail.com.com>
+-- Copyright   : (c) 2013 Brendan Hay <brendan.g.hay@gmail.com>
 -- License     : This Source Code Form is subject to the terms of
 --               the Mozilla Public License, v. 2.0.
 --               A copy of the MPL can be found in the LICENSE file or
