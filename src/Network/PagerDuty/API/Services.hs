4e3382835b021f9b960adfd685c5e645e7b25507
Brendan Hay <brendan.g.hay@gmail.com>
2014-09-27 18:58:20 +0200

    Updating licensing information

--- src/Network/PagerDuty/API/Services.hs
+++ src/Network/PagerDuty/API/Services.hs
@@ -2,6 +2,16 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings          #-}
 
+-- Module      : Network.PagerDuty.API.Services
+-- Copyright   : (c) 2013-2014 Brendan Hay <brendan.g.hay@gmail.com>
+-- License     : This Source Code Form is subject to the terms of
+--               the Mozilla Public License, v. 2.0.
+--               A copy of the MPL can be found in the LICENSE file or
+--               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+
 module Network.PagerDuty.API.Services
     ( ServiceDetails  (..)
     , CreateService   (..)
