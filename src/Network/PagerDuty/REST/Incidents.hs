da61a2ae6af1fc892c984634f2c5cb2dff1e2014
Brendan Hay <brendan.g.hay@gmail.com>
2014-10-24 11:33:02 +0200

    Correcting haddock markup

--- src/Network/PagerDuty/REST/Incidents.hs
+++ src/Network/PagerDuty/REST/Incidents.hs
@@ -436,12 +436,12 @@ cSince = upd.cSince'.mapping _D
 
 -- | The end of the date range over which you want to search.
 --
--- /Note: If you leave off either since or until, a 30 day default range is
+-- /Note:/ If you leave off either since or until, a 30 day default range is
 -- applied to your open ended range. Not including the since parameter will set
--- the date range to until - 30 days. Likewise, if you leave off until,
--- it is set to since + 30 days.
+-- the date range to until @-30@ days. Likewise, if you leave off until,
+-- it is set to since @+30@ days.
 --
--- Defaults to the last 30 days if you leave off both. The size of the date
+-- /Default:/ The last 30 days if you leave off both. The size of the date
 -- range must be less than 180 days.
 cUntil :: Lens' (Request CountIncidents s b) (Maybe UTCTime)
 cUntil = upd.cUntil'.mapping _D
